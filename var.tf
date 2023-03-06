2dd51be25db851ff1dd6a3dd4bb8c74b280cff37
Prakash Kumar <34745120+prakashk0301@users.noreply.github.com>
2023-03-06 16:15:02 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -4,12 +4,12 @@ variable "aws_vpc" {
 }
 
 variable "aws_subnet" {
-    description = "aws subnet"
+    description = "aws subnet1a"
     default = "subnet-0325154992ec9b4c5"
 }
 
 variable "aws_subnet2" {
-    description = "aws subnet2"
+    description = "aws subnet1b"
     default = "subnet-0fc8213f59cffd5fd"
 }
 
@@ -21,4 +21,9 @@ variable "aws_key" {
  variable "aws_ami" {
      description = "ami id"
      default = "ami-09439f09c55136ecf"
+}
+
+ variable "instance_type" {
+     description = "instance_type"
+     default = "t2.micro"
 }
