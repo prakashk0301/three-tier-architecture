50d6101d8d3f1a5a3251052448bb9a1779f40c51
Prakash Kumar <34745120+prakashk0301@users.noreply.github.com>
2023-03-06 16:09:54 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -38,12 +38,6 @@ resource "aws_lb_target_group_attachment" "target2" {
   port             = 80
   }
 
-#resource "aws_lb_target_group_attachment" "target2" {
-#  target_group_arn = "${aws_lb_target_group.target-grp.arn}"
-#  target_id        = "${aws_instance.win_vm.id}"
-#  port             = 80
-#  }
-
 resource "aws_lb_listener" "elb_listener" {
   load_balancer_arn = "${aws_lb.http_elb.arn}"
   port              = "80"
